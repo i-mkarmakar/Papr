987302bb74a780097b597b3b526f07386d8fdd84
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-07-11 15:46:34 +0530

    feat: add layout components including AppOptions, Header, SidebarClient, SidebarContent, SidebarLink, SidebarProvider, and Separator for improved UI structure

---
+++ src/components/layout/sidebar.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+import type { ReactNode } from "react";
+
+interface SidebarProps {
+  isOpen: boolean;
+  children: ReactNode;
+}
+
+const SidebarClient = ({ isOpen, children }: SidebarProps) => {
+  return (
+    <div className={cn("relative flex h-screen")}>
+      <div
+        className={cn(
+          "relative w-screen overflow-y-auto",
+          "bg-zinc-50 dark:bg-zinc-900",
+          "transition-all duration-300 ease-in-out",
+          isOpen ? "ml-60" : "ml-0",
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default SidebarClient;
