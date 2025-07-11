8c7f7dd0c5b8eaaf6be8f250774ff9d851541c96
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-07-11 11:33:41 +0530

    chore: update appConfig metadata

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const appConfig = {
+  title: "Papr",
+  description: "A minimalistic web app to remember & organize your things.",
+  prodUrl: "",
+  repositoryUrl: "https://github.com/i-mkarmakar/papr",
+};
