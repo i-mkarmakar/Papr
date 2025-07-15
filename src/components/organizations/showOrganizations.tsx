ad050f8c4c608fb2c3b7efb856994044c3b1a7b9
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-07-15 11:31:28 +0530

    feat: implement useIsMobile hook for responsive design and remove Skeleton component

--- src/components/organizations/showOrganizations.tsx
+++ src/components/organizations/showOrganizations.tsx
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { SidebarLink } from "@/components/layout/sidebarLink";
 import { getOrganizations } from "@/server/queries/client";
 import OrganizationOptions from "@/components/organizations/organizationOptions";
-import { Skeleton } from "../ui/skeleton";
 import { cn } from "@/lib/utils";
 
 interface ShowOrganizationsProps {
@@ -12,22 +11,17 @@ interface ShowOrganizationsProps {
 const ShowOrganizations = ({ userId }: ShowOrganizationsProps) => {
   const { data: organizations } = useQuery({
     queryKey: ["organizations"],
-    queryFn: () =>
-      getOrganizations({
-        userId,
-      }),
+    queryFn: () => getOrganizations({ userId }),
   });
 
-  if (!organizations) {
-    return <Skeleton className="h-8 w-full" />;
-  }
+  if (!organizations) return null;
 
   return organizations.map((org) => (
     <div
       key={org.slug}
       className={cn(
         "group flex items-center gap-2 px-4",
-        "animate-in fade-in slide-in-from-bottom-2 duration-200",
+        "animate-in fade-in slide-in-from-bottom-2 duration-200"
       )}
     >
       <SidebarLink href={`/app/${org.slug}`} title={org.name}>
